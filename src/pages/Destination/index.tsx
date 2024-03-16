af245900199434743cf0cfffeec17f89c0042b43
Viviane Araujo <developer.viviane@gmail.com>
2024-03-16 14:47:36 -0300

    firs commit

---
+++ src/pages/Destination/index.tsx
@@ -0,0 +1,15 @@
+import Navbar from '../../components/Navbar'
+import './Destination.modules.scss'
+
+const Destination = () => {
+
+    return (
+        <section className="destination-container">
+            <header>
+                <Navbar />
+            </header>
+        </section>
+    )
+}
+
+export default Destination
---
+++ src/pages/Technology/index.tsx
@@ -0,0 +1,16 @@
+import './Technology.modules.scss'
+import Navbar from '../../components/Navbar'
+
+
+const Technology = () => {
+
+    return (
+        <section className="technology-container">
+            <header>
+                <Navbar />
+            </header>
+        </section>
+    )
+}
+
+export default Technology
