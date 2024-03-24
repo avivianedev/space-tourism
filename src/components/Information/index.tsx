caf00e25a1a2097003ecfcb6744b3d2d1b7d45d3
Viviane Araujo <developer.viviane@gmail.com>
2024-03-23 21:26:32 -0300

    Implementado link do componente Navbar para dispositivo móvel

--- src/components/Information/index.tsx
+++ src/components/Information/index.tsx
@@ -1,38 +1,54 @@
 import './Information.modules.scss'
 import data from '../../data/planets.data.json'
-
+import { useState } from 'react'
 
 const Information = () => {
 
+    const [id, SetID] = useState('moon')
+
+    const getIdList = (e: any) => {
+        e.preventDefault()
+        SetID(e.target.text)
+
+    }
+
     return (
         <section className="info-container">
-            <div className="image-planet">
-                <img src="../src/assets/destination/image-moon.png" alt="image moon" />
 
-            </div>
-            <div className="planets-content-text">
-                <ul className="planets-list">
-                    <li className="planets-list-items"><a href="">moon</a></li>
-                    <li className="planets-list-items"><a href="">mars</a></li>
-                    <li className="planets-list-items"><a href="">europe</a></li>
-                    <li className="planets-list-items"><a href="">titans</a></li>
-                </ul>
-                <h1>moon</h1>
-                <p>See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.</p>
-                <div className="line">
-                    <hr />
-                </div>
-                <div className="planet-distance-travel-container">
-                    <div className='planet-distance-travel-content'>
-                        <p>avg. distance</p>
-                        <h3>384,400 km</h3>
+            {data.map((i: any) => (
+                <>
+                    <div className="image-planet">
+                        <img src={i[id]['image']} alt="image moon" />
+
                     </div>
-                    <div className='planet-distance-travel-content'>
-                        <p>est. travel time</p>
-                        <h3>3 days</h3>
+
+                    <div className="planets-content-text">
+                        <ul className="planets-list">
+                            <li onClick={(e) => { e.preventDefault(); SetID('moon') }} className="planets-list-items"><a href="">moon</a></li>
+                            <li onClick={(e) => { e.preventDefault(); SetID('mars') }} className="planets-list-items"><a href="">mars</a></li>
+                            <li onClick={(e) => { e.preventDefault(); SetID('europe') }} className="planets-list-items"><a href="">europe</a></li>
+                            <li onClick={(e) => { e.preventDefault(); SetID('titan') }} className="planets-list-items"><a href="">titans</a></li>
+                        </ul>
+                        <h1>{i[id]['title']}</h1>
+                        <p>{i[id]['description']}</p>
+                        <div className="line">
+                            <hr />
+                        </div>
+                        <div className="planet-distance-travel-container">
+                            <div className='planet-distance-travel-content'>
+                                <p>avg. distance</p>
+                                <h3>{i[id]['distance']}</h3>
+                            </div>
+                            <div className='planet-distance-travel-content'>
+                                <p>est. travel time</p>
+                                <h3>{i[id]['travel']}</h3>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
+                </>
+            ))}
+
+
         </section>
     )
 }
